8ba53ffd2718138109b62874440be180cca45b73
L1ghtDream <radu.voinea.mihai@gmail.com>
2022-03-18 04:03:14 +0200

    Fixed version

--- src/main/java/dev/lightdream/logger/LoggableMain.java
+++ src/main/java/dev/lightdream/logger/LoggableMain.java
@@ -4,7 +4,7 @@ public interface LoggableMain {
 
     @SuppressWarnings("unused")
     static String getVersion() {
-        return "Logger " + LoggableMain.class.getPackage().getImplementationVersion();
+        return "Logger 2.2.5";
     }
 
     boolean debug();
