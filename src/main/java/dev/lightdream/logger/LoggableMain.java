405d79f7a05d71841d702dbfdc88bce71af8ca2e
L1ghtDream <radu.voinea.mihai@gmail.com>
2022-03-18 03:49:36 +0200

    Added version print-out

--- src/main/java/dev/lightdream/logger/LoggableMain.java
+++ src/main/java/dev/lightdream/logger/LoggableMain.java
@@ -2,6 +2,11 @@ package dev.lightdream.logger;
 
 public interface LoggableMain {
 
+    @SuppressWarnings("unused")
+    static String getVersion() {
+        return "Logger " + LoggableMain.class.getPackage().getImplementationVersion();
+    }
+
     boolean debug();
 
     void log(String log);
