4f8ea303784ff18ee1bb0d1949b4d194fd359a42
L1ghtDream <radu.voinea.mihai@gmail.com>
2022-04-06 20:11:20 +0300

    Removed getVersion()

--- src/main/java/dev/lightdream/logger/LoggableMain.java
+++ src/main/java/dev/lightdream/logger/LoggableMain.java
@@ -2,11 +2,6 @@ package dev.lightdream.logger;
 
 public interface LoggableMain {
 
-    @SuppressWarnings("unused")
-    static String getVersion() {
-        return "Logger 2.2.6\n";
-    }
-
     boolean debug();
 
     void log(String log);
