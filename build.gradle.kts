a65f7d518bcc323f9a09f8a12710a8fed302414e
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-01-16 23:19:25 +0200

    Added publication target

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,6 +55,10 @@ publishing {
         val githubUsername = project.findProperty("github.auth.username") ?: ""
         val githubPassword = project.findProperty("github.auth.password") ?: ""
 
+        val selfURL = project.findProperty("self.url") ?: ""
+        val selfUsername = project.findProperty("self.auth.username") ?: ""
+        val selfPassword = project.findProperty("self.auth.password") ?: ""
+
         maven(url = gitlabURL as String) {
             name = "gitlab"
             credentials(HttpHeaderCredentials::class) {
@@ -73,6 +77,14 @@ publishing {
                 password = githubPassword as String
             }
         }
+
+        maven(url = selfURL as String) {
+            name = "self"
+            credentials(PasswordCredentials::class) {
+                username = selfUsername as String
+                password = selfPassword as String
+            }
+        }
     }
 }
 
@@ -84,4 +96,9 @@ tasks.register("publishGitLab") {
 tasks.register("publishGitHub") {
     dependsOn("publishMavenPublicationToGithubRepository")
     description = "Publishes to GitHub"
+}
+
+tasks.register("publishSelf") {
+    dependsOn("publishMavenPublicationToSelfRepository")
+    description = "Publishes to Self hosted repository"
 }
