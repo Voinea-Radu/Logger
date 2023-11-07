a16b3f0eea4cb2124ca1fd1b7d240e7f69667365
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-11-07 20:55:24 +0200

    Updated use of @Builder

--- src/main/java/dev/lightdream/logger/Printer.java
+++ src/main/java/dev/lightdream/logger/Printer.java
@@ -1,6 +1,6 @@
 package dev.lightdream.logger;
 
-import lombok.Builder;
+import lombok.Builder.Default;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
@@ -10,7 +10,6 @@ import org.jetbrains.annotations.Nullable;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-@Builder(builderClassName = "_Builder", toBuilder = true)
 @Getter
 @Setter
 @Accessors(chain = true, fluent = true)
@@ -20,22 +19,27 @@ public class Printer {
         builder().build();
     }
 
-    @lombok.Builder.Default
-    private boolean debugger = false;
-    @lombok.Builder.Default
     @NotNull
-    private String logFilesFolder = "/logs/";
-    @lombok.Builder.Default
+    private @Default String logFilesFolder = "/logs/";
     @NotNull
-    private String debugFilesFolder = "/debug/";
-    @lombok.Builder.Default
-    private boolean logTime = false;
-    @lombok.Builder.Default
-    private boolean logToFile = false;
-    @lombok.Builder.Default
-    private boolean debugToFile = false;
-    @lombok.Builder.Default
-    private boolean debugToConsole = false;
+    private @Default String debugFilesFolder = "/debug/";
+    private @Default boolean logTime = false;
+    private @Default boolean logToFile = false;
+    private @Default boolean debugToFile = false;
+    private @Default boolean debugToConsole = false;
+    private @Default boolean debugger = false;
+
+    @lombok.Builder(builderClassName = "_Builder", toBuilder = true)
+    private Printer(@NotNull String logFilesFolder, @NotNull String debugFilesFolder, boolean logTime,
+                    boolean logToFile, boolean debugToFile, boolean debugToConsole, boolean debugger) {
+        this.debugger = debugger;
+        this.logFilesFolder = logFilesFolder;
+        this.debugFilesFolder = debugFilesFolder;
+        this.logTime = logTime;
+        this.logToFile = logToFile;
+        this.debugToFile = debugToFile;
+        this.debugToConsole = debugToConsole;
+    }
 
     public static Builder builder() {
         return new Builder();
@@ -50,58 +54,28 @@ public class Printer {
         return true;
     }
 
-    /**
-     * Print a white message
-     *
-     * @param object The object to print
-     */
     public void info(@Nullable Object object) {
         info(object, ConsoleColor.WHITE);
     }
 
-    /**
-     * Print a white message
-     *
-     * @param object The object to print
-     */
     public void info(@Nullable Object object, @NotNull ConsoleColor color) {
         if (!checks(object)) return;
         //noinspection DataFlowIssue
         log(color + object.toString() + ConsoleColor.RESET, debugger);
     }
 
-    /**
-     * {@link Logger#info} alias
-     *
-     * @param object The object to print
-     */
     public void log(@Nullable Object object) {
         info(object);
     }
 
-    /**
-     * Print a red message
-     *
-     * @param object The object to print
-     */
     public void error(@Nullable Object object) {
         info(object, ConsoleColor.RED);
     }
 
-    /**
-     * Print a green message
-     *
-     * @param object The object to print
-     */
     public void good(@Nullable Object object) {
         info(object, ConsoleColor.GREEN);
     }
 
-    /**
-     * Print a yellow message
-     *
-     * @param object The object to print
-     */
     public void warn(@Nullable Object object) {
         info(object, ConsoleColor.YELLOW);
     }
@@ -139,8 +113,10 @@ public class Printer {
         public Printer build() {
             // We need to call it multiple times to get multiple instances
             Printer output = super.build();
+
             this.debugger(false);
             Printer logPrinter = super.build();
+
             this.debugger(true);
             Printer debugPrinter = super.build();
 
